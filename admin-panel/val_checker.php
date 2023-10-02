0c9d03ec57d1d014c0fbfd82fc12a126ea50d53c
Raj <gamingmodmania@gmail.com>
2023-10-02 18:33:48 +0530

    Third commit: change db file

--- admin-panel/val_checker.php
+++ admin-panel/val_checker.php
@@ -2,14 +2,14 @@
 
 require_once './SDC.php';
 
-if($_SERVER['REQUEST_METHOD'] == 'POST'){
- 
-    if(!empty($_POST['u_email'])){
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    if (!empty($_POST['u_email'])) {
         $EMAIL = $_POST['u_email'];
         $EMAIL = crcheck($EMAIL);
         cemail($EMAIL);
-    } 
-    if(!empty($_POST['u_fname']) || !empty($_POST['u_lname'])){
+    }
+    if (!empty($_POST['u_fname']) || !empty($_POST['u_lname'])) {
         $FNAME = $_POST['u_fname'];
         $LNAME = $_POST['u_lname'];
 
@@ -18,40 +18,43 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
 
         cname($FNAME, $LNAME);
     }
-    if(!empty($_POST['veri_code'])){
+    if (!empty($_POST['veri_code'])) {
         $code = $_POST['veri_code'];
         $code_email = $_POST['veri_email'];
         crcheck($code);
         crcheck($code_email);
         code_veri($code, $code_email);
-    } 
-    if(!empty($_POST['u_username'])){
+    }
+    if (!empty($_POST['u_username'])) {
         $us_name = $_POST['u_username'];
         crcheck($us_name);
         c_user($us_name);
     }
 }
 
-function cname($fname, $lname = ''){
-    if (!preg_match("/^[a-zA-Z-' ]*$/",$fname)) {
+function cname($fname, $lname = '')
+{
+    if (!preg_match("/^[a-zA-Z-' ]*$/", $fname)) {
         msgSender("Only letters and white space allowed", 'w');
-    } else if(!preg_match("/^[a-zA-Z-' ]*$/", $lname)){
+    } else if (!preg_match("/^[a-zA-Z-' ]*$/", $lname)) {
         msgSender("Only letters and white space allowed", 'w');
-    } else{
+    } else {
         msgSender('Verify', 's');
     }
 }
 
-function cemail($email){
+function cemail($email)
+{
     if (!filter_var($email, FILTER_VALIDATE_EMAIL) && (str_contains($email, '@gmail.com') || str_contains($email, '@yahoo.com') || str_contains($email, '@outlook.com') || str_contains($email, '@hotmail.com') || str_contains($email, '@'))) {
         $nameErr = "Invalid email format";
         msgSender($nameErr, 'w');
-    } else{
+    } else {
         email_Sender($email);
     }
 }
 
-function crcheck($data){
+function crcheck($data)
+{
     $data = trim($data);
     $data = stripslashes($data);
     $data = htmlspecialchars($data);
@@ -59,22 +62,24 @@ function crcheck($data){
     return $data;
 }
 
-function msgSender($MSG, $FLAG){
+function msgSender($MSG, $FLAG)
+{
     $msgArr = array(
-        "msg" => $MSG, 
+        "msg" => $MSG,
         "flag" => $FLAG
     );
-    if($FLAG == 's'){
+    if ($FLAG == 's') {
         echo json_encode($msgArr);
         exit;
-    } else if($FLAG == 'w'){
+    } else if ($FLAG == 'w') {
         echo json_encode($msgArr);
     }
 }
 
-function email_Sender($to){
+function email_Sender($to)
+{
 
-    $ocode = rand(100000,999999);
+    $ocode = rand(100000, 999999);
 
     $otime = time();
 
@@ -83,31 +88,31 @@ function email_Sender($to){
     $header = "MIME-Version: 1.0\r\nContent-Type: text/html;charset=UTF-8\r\n";
     $header .= "From: learnbeyondthink@gmail.com";
 
-    $msg =  '<main style="display: flex; align-content : center; justify-content:center; width : 100%; height : auto; border-radius : 10px;background: #F6F6F2; margin: 0px; padding: 10px 0px;">' .
-                '<section style="width : 100%; max-width : 400px; box-shadow : 0px 0px 5px 0px black; height: auto; min-height: 400px; margin: 0px; padding: 15px; border-radius: 10px;">' .
-                    '<h1>BLOGDAY.ONLINE</h1>' .
-                    '<h2>Verify Your Email</h2>' .
-                    '<p>Here is your Email verification code.</p>' .
-                    '<div style="background: silver; padding: 10px; text-align: center; border-radius: 10px; font-size : 25px; font-weight : bold; color : #151210;">' .
-                    $ocode .
-                    '</div>' .
-                    '<p>Please make sure you never share this code to anyone.</p>' .
-                    '<ol>' .
-                        '<strong>Note:</strong>' .
-                        '<li>This code will expire in 15 minutes.</li>' .
-                        '<li>If you not generate this code. Please Ignore this Email.</li>' .
-                    '</ol>' .
-                    '<p>~Thanks for using~ <a href="//blogday.online">Blogday.online</a></p>' .
-                '</section>' .
-            '</main>';
+    $msg = '<main style="display: flex; align-content : center; justify-content:center; width : 100%; height : auto; border-radius : 10px;background: #F6F6F2; margin: 0px; padding: 10px 0px;">' .
+        '<section style="width : 100%; max-width : 400px; box-shadow : 0px 0px 5px 0px black; height: auto; min-height: 400px; margin: 0px; padding: 15px; border-radius: 10px;">' .
+        '<h1>BLOGDAY.ONLINE</h1>' .
+        '<h2>Verify Your Email</h2>' .
+        '<p>Here is your Email verification code.</p>' .
+        '<div style="background: silver; padding: 10px; text-align: center; border-radius: 10px; font-size : 25px; font-weight : bold; color : #151210;">' .
+        $ocode .
+        '</div>' .
+        '<p>Please make sure never share this code to anyone.</p>' .
+        '<ol>' .
+        '<strong>Note:</strong>' .
+        '<li>This code will expire in 15 minutes.</li>' .
+        '<li>If you not generate this code. Please Ignore this Email.</li>' .
+        '</ol>' .
+        '<p>~Thanks for using~ <a href="//blogday.online">Blogday.online</a></p>' .
+        '</section>' .
+        '</main>';
 
     $selectQuery = "INSERT INTO email_verify (UEMAIL, UCODE, UTIME) values('$to', '$ocode', '$otime')";
 
-    if(mysqli_query($GLOBALS['connn'], $selectQuery)){
-        if(mail($to, $sj, $msg, $header)){
+    if (mysqli_query($GLOBALS['connn'], $selectQuery)) {
+        if (mail($to, $sj, $msg, $header)) {
             msgSender('Message Sent Successfully, Check Inbox', 's');
-        } else{
-            msgSender('Something went wrong, Try again', 'w');  
+        } else {
+            msgSender('Something went wrong, Try again', 'w');
         }
     } else {
         msgSender('Something went wrong, Try again', 'w');
@@ -115,38 +120,40 @@ function email_Sender($to){
 
 }
 
-function code_veri($code, $code_email){
+function code_veri($code, $code_email)
+{
 
     $sq = "SELECT * FROM email_verify where UEMAIL='$code_email'";
     $my_sq = mysqli_query($GLOBALS['connn'], $sq);
 
-    if(mysqli_fetch_row($my_sq) > 0){
+    if (mysqli_fetch_row($my_sq) > 0) {
         $r_d = mysqli_fetch_assoc($my_sq);
         $t_rd = $r_d['UTIME'];
         $c_rd = $r_d['UCODE'];
-        
+
         $c_t = time();
         $d_t = $c_t - $t_rd;
-        if($d_t > 900){
+        if ($d_t > 900) {
             msgSender('Code is expired, Resend it', 'w');
-        } else if($d_t < 900){
-            if($c_rd == $code){
+        } else if ($d_t < 900) {
+            if ($c_rd == $code) {
                 msgSender('Email is verified', 's');
             } else {
                 msgSender('Code is invalid', 'w');
             }
         }
-    } else{
+    } else {
         msgSender('Something went wrong', 'w');
     }
 }
 
-function c_user($un){
+function c_user($un)
+{
     $sq02 = "SELECT * FROM bdo_x_y_z_user where bd_username='$un'";
     $my_sq03 = mysqli_query($GLOBALS['connn'], $sq02);
-    if(mysqli_fetch_row($my_sq03) > 0){
+    if (mysqli_fetch_row($my_sq03) > 0) {
         msgSender('Username is exists, Try another', 'w');
-    } else{
+    } else {
         msgSender('Created', 's');
     }
 }
--- pwapi/index.php
+++ pwapi/index.php
@@ -2,16 +2,16 @@
 
 include_once("./SDC.php");
 
-if($_SERVER['REQUEST_METHOD'] == "GET" && ($_GET["q"] == "rcnt" || $_GET["q"] == "pplr" || $_GET["q"] == "aig" || $_GET["q"] == "rltv" || $_GET["q"] == "ctgr" || $_GET["q"] == "wall")){
-    
+if ($_SERVER['REQUEST_METHOD'] == "GET" && ($_GET["q"] == "rcnt" || $_GET["q"] == "pplr" || $_GET["q"] == "aig" || $_GET["q"] == "rltv" || $_GET["q"] == "ctgr" || $_GET["q"] == "wall")) {
+
     // Recent Wallpaper
     // Popular Wallpaper
     // Ai Generated Wallpaper
     // Relative Wallpaper
     // Category of Wallpaper
     // Single Wallpaper Detail using id
-    
-} else{
+
+} else {
     header('HTTP/1.1 404 Not Found');
 }
 
