48a8fa819fbfdf62c8dc11cfa00ee975e713a6c9
Raj <gamingmodmania@gmail.com>
2023-10-24 17:48:39 +0530

    update AFPBA 17:48 24-10-23

--- api/apiForPixelBlazeApp.php
+++ api/apiForPixelBlazeApp.php
@@ -57,6 +57,7 @@ if ($_SERVER['SERVER_NAME'] == 'aiwallpaper.online' && ($_SERVER["REQUEST_METHOD
             $_resDB_00 = mysqli_query($connection, $_queryDB_00);
             $_resDB_00 = mysqli_fetch_array($_resDB_00);
             $_starts_from_id = $_resDB_00['id'];
+            echo json_encode($_resDB_00);
         }
 
         $_queryDB = "SELECT * FROM wallpaperaccess ORDER BY 1 DESC LIMIT $_starts_from_id, $_how_much_need";
